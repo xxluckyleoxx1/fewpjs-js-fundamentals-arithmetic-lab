f6ad23999e553bee6d29f7e3ceafa0f56ef62f8d
Leonides <xxluckyleoxx1@gmail.com>
2021-04-05 11:42:07 -0400

    commit tree 1ee5870f20b4deace800fa246ba0d4e7bdcc616a

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 6;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10)+1;
+let mod = num3 % num4;
+let max = Math.max(5, 10, 15, 20);
